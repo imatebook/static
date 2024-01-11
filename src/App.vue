ffcc8f9cbbac892ac3e881f479167ad804b3cd72
wangjingpei <wangjingpei@iyoo.com>
2024-01-11 11:24:56 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
+    <img alt="blog logo" class="logo" src="/blog/logo.png" width="125" />
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
 
